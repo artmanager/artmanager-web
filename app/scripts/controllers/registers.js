da71389abcd33cac3e3ac8f18419f39a0a40408e
Gustavo Oliveira <Gustavo Oliveira>
2016-06-13 02:23:51 -0300

    cadastro de usuário criado

--- app/scripts/controllers/registers.js
+++ app/scripts/controllers/registers.js
@@ -1,37 +1,67 @@
-﻿angular.module('artmanager')
-  .controller('DashboardCtrl', function ($scope, $http, $location, $state, $window) {
+﻿'use strict';
 
-        if ($window.sessionStorage.token == null || $window.sessionStorage.token == undefined) {
-            $location.path('/login');
-        }
+angular.module('artmanager')
+    .controller('RegisterCtrl', function ($scope, $http, $location, $state, $window,$mdDialog, $mdMedia) {
+
+    $scope.user = {
+        user: '',
+        name: '',
+        password: '',
+        confirm: ''
+    };
         
-        $scope.user = {
-            login: '',
-            password: '',
-            confirm: ''
+    $scope.RegisterUser = function (o, r) {
+        try {
+            var obj = {
+                user: $scope.user
+            } 
+            if (obj.password == '' || (obj.password != obj.confirm)) {
+                  $mdDialog.show(
+                    $mdDialog.alert()
+                        .clickOutsideToClose(true)
+                        .title('Erro')
+                        .textContent('Por favor preencha todos os campos.')
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('OK')
+                );
+            }
+            $http({
+                method: 'POST',
+                url: 'http://api.artmanager.com.br/users',
+                data: obj,
+                headers: { 'x-access-token': $window.sessionStorage.token }
+            }).then(function successCallback(response) {
+                var data = response.data;
+                if (data.success) {
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                        .clickOutsideToClose(true)
+                        .title('Sucesso')
+                        .textContent(data.success)
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('OK')
+                    );
+                } else {
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                        .clickOutsideToClose(true)
+                        .title('Erro')
+                        .textContent(data.error)
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('OK')
+                    );
+                }
+
+                $scope.name = '';
+                $scope.user = '';
+                $scope.password = '';
+                $scope.confirm = '';
+
+            }, function errorCallback(response) {
+                delete $window.sessionStorage.token;
+            });
+        } catch (e) {
+            console.log(e);
         }
-
-        $scope.registerUser = function () {
-
-        if ($scope.user.password != $scope.confirm) {
-            alert('Senhas invalidas');
-            return false;
-        }
-
-        var obj = {
-            user: $scope.user,
-            password: $scope.password
-        };
-
-        $http({
-            method: 'POST',
-            url: 'http://api.artmanager.com.br/users',
-            data: obj
-        })
-        .success(function (success) {
-
-        })
-        .error(function () {
-        });
-      }
-  })
+    };
+});
